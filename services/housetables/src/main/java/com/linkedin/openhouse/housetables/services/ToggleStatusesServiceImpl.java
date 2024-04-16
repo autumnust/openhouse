cd3c07c3521f89549617900a633c277bb1ad701a
Lei Sun <lesun@linkedin.com>
2024-04-15 17:50:09 -0700

    Part II: Adding regex-based rule engine

--- services/housetables/src/main/java/com/linkedin/openhouse/housetables/services/ToggleStatusesServiceImpl.java
+++ services/housetables/src/main/java/com/linkedin/openhouse/housetables/services/ToggleStatusesServiceImpl.java
@@ -3,15 +3,21 @@ package com.linkedin.openhouse.housetables.services;
 import com.linkedin.openhouse.common.api.spec.ToggleStatusEnum;
 import com.linkedin.openhouse.housetables.api.spec.model.ToggleStatus;
 import com.linkedin.openhouse.housetables.repository.impl.jdbc.ToggleStatusHtsJdbcRepository;
+import com.linkedin.openhouse.housetables.services.toggle.TableRuleEngine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ToggleStatusesServiceImpl implements ToggleStatusesService {
   @Autowired ToggleStatusHtsJdbcRepository htsRepository;
+  @Autowired TableRuleEngine tableRuleEngine;
 
   @Override
   public ToggleStatus getTableToggleStatus(String featureId, String databaseId, String tableId) {
-    return ToggleStatus.builder().status(ToggleStatusEnum.ACTIVE).build();
+    boolean evalResult =
+        tableRuleEngine.eval(databaseId, tableId, htsRepository.findAllByFeature(featureId));
+    return evalResult
+        ? ToggleStatus.builder().status(ToggleStatusEnum.ACTIVE).build()
+        : ToggleStatus.builder().status(ToggleStatusEnum.INACTIVE).build();
   }
 }
